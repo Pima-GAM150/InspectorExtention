2b090ad2d08f43d15fe17257a5e486b9c7941a78
Nate Mendoza <nmendoza82rg@aol.com>
2019-04-03 19:07:15 -0700

    4th
    got it looking good and stuff goona work on the prefab maker next

--- CustomInspector/Assets/Editor/CreateParentObject.cs
+++ CustomInspector/Assets/Editor/CreateParentObject.cs
@@ -5,13 +5,14 @@ using UnityEditor.SceneManagement;
 public class CreateParentObject : EditorWindow
 {
 
-    string nameInput = "New Parent Object";
+    string nameInput = "(New Parent Object)";
 
 
-    bool twoDimObject = false;
+    bool sameZPlane = false;
     bool sameYPlane = false;
+    bool sameXPlane = false;
 
-    GameObject[] selection { get { return Selection.gameObjects; } }
+    GameObject[] selectedObjects { get { return Selection.gameObjects; } }
 
     [MenuItem("GameObject/Parent Maker")]
     public static void ShowWindow()
@@ -29,14 +30,14 @@ public class CreateParentObject : EditorWindow
         //these are the fields for the parent if it is a 2D object then the children will have matching z
         nameInput = EditorGUILayout.TextField("Name", nameInput);
 
-        twoDimObject = EditorGUILayout.Toggle(new GUIContent("2D Object", "2D Will have their z set to the New Parent"),twoDimObject);
-        sameYPlane = EditorGUILayout.Toggle(new GUIContent("Same Y Axis", "This will set all children's Y position to 0 with Parent"), sameYPlane);
-
+        sameZPlane = EditorGUILayout.Toggle(new GUIContent("Align Z Axis", "Aligns all children to parents Z axis (Good for 2D)"), sameZPlane);
+        sameYPlane = EditorGUILayout.Toggle(new GUIContent("Align Y Axis", "Aligns all children to parents Y axis (Good for 3D)"), sameYPlane);
+        sameXPlane = EditorGUILayout.Toggle(new GUIContent("Align X Axis", "Aligns all children to parents X axis"), sameXPlane);
 
 
 
         EditorGUILayout.LabelField("Currently Selected : ", EditorStyles.boldLabel);
-        foreach (GameObject gameObj in selection)
+        foreach (GameObject gameObj in selectedObjects)
         {
             EditorGUILayout.LabelField(gameObj.name);
             this.Repaint();
@@ -47,28 +48,36 @@ public class CreateParentObject : EditorWindow
 
         if (GUILayout.Button("Make Parent"))
         {
-        
+
             GameObject prefab = new GameObject(nameInput);
 
-            foreach (GameObject gameObj in selection)
+            if (selectedObjects != null)
             {
-                gameObj.transform.SetParent(prefab.transform);
-                if (twoDimObject)
+                foreach (GameObject gameObj in selectedObjects)
                 {
-                    gameObj.transform.position = new Vector3(gameObj.transform.position.x, gameObj.transform.position.y, prefab.transform.position.z);
+                    gameObj.transform.SetParent(prefab.transform);
+
+                    if (sameZPlane)
+                    {
+                        gameObj.transform.position = new Vector3(gameObj.transform.position.x, gameObj.transform.position.y, prefab.transform.position.z);
+                    }
+                    if (sameYPlane)
+                    {
+                        gameObj.transform.position = new Vector3(gameObj.transform.position.x, prefab.transform.position.y, gameObj.transform.position.z);
+                    }
+                    if (sameXPlane)
+                    {
+                        gameObj.transform.position = new Vector3(prefab.transform.position.x, gameObj.transform.position.y, gameObj.transform.position.z);
+                    }
                 }
-                if (sameYPlane)
-                {
-                    gameObj.transform.position = new Vector3(gameObj.transform.position.x, prefab.transform.position.y, gameObj.transform.position.z);
-                }
+
+                prefab.transform.position = new Vector3(prefab.GetComponentInChildren<Transform>().position.x, prefab.GetComponentInChildren<Transform>().position.y, prefab.GetComponentInChildren<Transform>().position.z);
+
+                EditorSceneManager.MarkAllScenesDirty();
             }
-
-            prefab.transform.position = new Vector3(0,0,0);
-
-            EditorSceneManager.MarkAllScenesDirty();
         }
 
-        //////////
+        /*
         if (GUILayout.Button("Make Prefab"))
         {  //Loop through every GameObject in the array above
             foreach (GameObject gameObject in objectArray)
@@ -107,5 +116,7 @@ public class CreateParentObject : EditorWindow
         //Create a new Prefab at the path given
         Object prefab = PrefabUtility.CreatePrefab(localPath, obj);
         PrefabUtility.ReplacePrefab(obj, prefab, ReplacePrefabOptions.ConnectToPrefab);
+    }
+    */
     }
 }
