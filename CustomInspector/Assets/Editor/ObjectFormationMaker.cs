57135667028c1d129b824e8e2e589d651a491669
Nate Mendoza <nmendoza82rg@aol.com>
2019-04-06 08:40:53 -0700

    2nd Script
    Adding a second editor script this one will take selected objects and arrange them in a formation

---
+++ CustomInspector/Assets/Editor/ObjectFormationMaker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ObjectFormationMaker : EditorWindow
+{
+    [MenuItem("GameObject/Formation Maker")]
+    public static void ShowWindow()
+    {
+        GetWindow<ObjectFormationMaker>("Formation Maker");
+    }
+
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Select Objects to put into a formation");
+    }
+
+}
