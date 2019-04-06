eaf5c311a0427701f43616d1fdc233af138f6a53
Nate Mendoza <nmendoza82rg@aol.com>
2019-04-06 09:33:57 -0700

    Got the formations working
    I got 4 buttons for 4 different formations
    line horizontal on x
    line on the z
    diagonal line on the x and z

--- CustomInspector/Assets/Editor/ObjectFormationMaker.cs
+++ CustomInspector/Assets/Editor/ObjectFormationMaker.cs
@@ -1,23 +1,83 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class ObjectFormationMaker : EditorWindow
 {
+
+    float distanceFromEachOther = 1;
+
+    GameObject[] selectedObjects { get { return Selection.gameObjects; } }
+
     [MenuItem("Extentions/Formation Maker")]
     public static void ShowWindow()
     {
         GetWindow<ObjectFormationMaker>("Formation Maker");
-    }
+    }//makes the window for the editor
 
 
     private void OnGUI()
-    {
-        GUILayout.Label("Select Objects to put into a formation", EditorStyles.boldLabel);
+    {//this draws evertything on the editor
 
-        if (GUILayout.Button("Put In Formation"))
+        //window header
+        GUILayout.Label(" Select objects to put into a formation \n Formations are based off the first selected object", EditorStyles.boldLabel);
+        distanceFromEachOther = EditorGUILayout.FloatField(distanceFromEachOther);
+
+
+        EditorGUILayout.LabelField("Currently Selected : ", EditorStyles.boldLabel);
+        foreach (GameObject gameObj in selectedObjects)
+        {
+            EditorGUILayout.LabelField(gameObj.name);
+            this.Repaint();
+        }
+
+        
+
+
+        if (GUILayout.Button("Vertical Stack"))
         {
 
+            for (int i = 1; i < selectedObjects.Length; i++)
+            {//so formations are based off the Y positions in the world
+                selectedObjects[i].transform.position = new Vector3(selectedObjects[i - 1].transform.position.x, selectedObjects[i - 1].transform.position.y + distanceFromEachOther, selectedObjects[i - 1].transform.position.z);
+            }
+            EditorSceneManager.MarkAllScenesDirty();
         }
+
+
+        if (GUILayout.Button("Horizontal Line on X Axis"))
+        {
+
+            for (int i = 1; i < selectedObjects.Length; i++)
+            {//so formations are based off the Y positions in the world
+                selectedObjects[i].transform.position = new Vector3(selectedObjects[i - 1].transform.position.x + distanceFromEachOther, selectedObjects[i - 1].transform.position.y, selectedObjects[i - 1].transform.position.z);
+            }
+
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+
+        if (GUILayout.Button("Horizontal Line on Z Axis"))
+        {
+
+            for (int i = 1; i < selectedObjects.Length; i++)
+            {//so formations are based off the Y positions in the world
+                selectedObjects[i].transform.position = new Vector3(selectedObjects[i - 1].transform.position.x, selectedObjects[i - 1].transform.position.y, selectedObjects[i - 1].transform.position.z + distanceFromEachOther);
+            }
+
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+
+        if (GUILayout.Button("Horizontal Digaonal Line"))
+        {
+
+            for (int i = 1; i < selectedObjects.Length; i++)
+            {//so formations are based off the Y positions in the world
+                selectedObjects[i].transform.position = new Vector3(selectedObjects[i - 1].transform.position.x + distanceFromEachOther, selectedObjects[i - 1].transform.position.y, selectedObjects[i - 1].transform.position.z + distanceFromEachOther);
+            }
+
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+
     }
 
 }
